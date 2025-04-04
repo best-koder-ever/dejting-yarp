c9a27914e40b21fc3ef0aa620bad1585453df169
best-koder-ever <dettaminnsjag@gmail.com>
2025-04-04 13:15:57 +0200

    Committing changes for dejting-yarp

--- dejting-yarp/Program.cs
+++ dejting-yarp/Program.cs
@@ -5,12 +5,27 @@ using Yarp.ReverseProxy;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddReverseProxy()
                 .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
+
+// Use CORS policy
+app.UseCors("AllowAll");
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
